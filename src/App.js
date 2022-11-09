b35d4d0470804fea265b9ae37c632234fc9b54a0
shorif <sashipon001@gmail.com>
2022-11-09 22:57:24 +0600

    new youtube link added in appjs file

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
   );
 }
 // https://www.youtube.com/watch?v=HPBuLS04ojE&t=5690s&ab_channel=StepbyStep
-
 // https://www.youtube.com/watch?v=TbGnq5CBPXY&ab_channel=Taversity
 // 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
 
